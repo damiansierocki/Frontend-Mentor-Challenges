89ce9e3dfbe8bbd413089f6298cce2658b73bca3
damiansierocki <damiansierocki7@gmail.com>
2022-09-19 13:55:36 +0200

    Newbie - Interactive rating component: Add html, style and js for first window

--- Newbie/interactive-rating-component-main/script.js
+++ Newbie/interactive-rating-component-main/script.js
@@ -1,1 +1,16 @@
-console.log('test');
+const ratingsButtons = document.querySelectorAll('.buttons-container__button');
+let rating;
+
+const removeClickedClassFromButtons = () => {
+	ratingsButtons.forEach((item) => {
+		item.classList.remove('clicked');
+	});
+};
+
+ratingsButtons.forEach((item) => {
+	item.addEventListener('click', function () {
+		rating = Number(item.textContent.trim());
+		removeClickedClassFromButtons();
+		item.classList.add('clicked');
+	});
+});
