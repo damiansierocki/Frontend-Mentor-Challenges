ef6ccc4eff3eda5d2520236ab0fb703b47224367
damiansierocki <damiansierocki7@gmail.com>
2022-09-19 15:33:13 +0200

    Newbie - Interactive rating component: Completion of the project

--- Newbie/interactive-rating-component-main/script.js
+++ Newbie/interactive-rating-component-main/script.js
@@ -1,4 +1,9 @@
+const cardContainer = document.querySelector('.card');
+const thankyouContainer = document.querySelector('.thank-you');
 const ratingsButtons = document.querySelectorAll('.buttons-container__button');
+const submitButton = document.querySelector('.card__button-submit');
+const scoreUser = document.querySelector('.score__user');
+const scoreMax = document.querySelector('.score__max');
 let rating;
 
 const removeClickedClassFromButtons = () => {
@@ -13,4 +18,13 @@ ratingsButtons.forEach((item) => {
 		removeClickedClassFromButtons();
 		item.classList.add('clicked');
 	});
+});
+
+submitButton.addEventListener('click', function () {
+	cardContainer.style.display = 'none';
+
+	scoreUser.textContent = rating;
+	scoreMax.textContent = 5;
+
+	thankyouContainer.style.display = 'block';
 });
